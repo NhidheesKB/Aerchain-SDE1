51bc2631ea9f49a0af07105267580d151ef8474a
NhidheesKB <nhidhees@gmail.com>
2025-12-03 23:55:31 +0530

    deleted file

--- backend/src/service/task_service.ts
+++
@@ -1,9 +0,0 @@
-import knex from "knex";
-import db from '../../config/database.js'
-export class TaskService{
-
-    async getTask(){
-        const tasks=await db.select('*').from('task');
-        return tasks
-    }
-}
